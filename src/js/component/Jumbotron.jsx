47a1194fc8316f423025c8883828892ab107e3d0
Lucrecia-15 <parodilucrecia@gmail.com>
2024-12-09 21:36:21 +0000

    First commit

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div class="container py-4 bg-light text-dark " bis_skin_checked="1">
-      <h1 class="col-md-8 display-8 fs-1 d-md-flex justify-content-md-start">Welcome to... ! </h1>
-      <p class="text-start col-md-10 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-      <button class="d-md-flex justify-content-md-end btn btn-primary py-2 btn-lg" type="button">Call to action!</button>
+    <div className="container py-4 bg-light text-dark " bis_skin_checked="1">
+      <h1 className="col-md-8 display-8 fs-1 d-md-flex justify-content-md-start">Welcome to... ! </h1>
+      <p className="text-start col-md-10 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+      <button className="d-md-flex justify-content-md-end btn btn-primary py-2 btn-lg" type="button">Call to action!</button>
     </div>   
    
   )
